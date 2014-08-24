4bcbaf3f4be7d4b19984aa102e53ed501268de64
Karin <karin@susannah>
2014-08-24 13:27:54 -0700

    completed app

--- server.R
+++ server.R
@@ -1,75 +1,99 @@
 library(shiny)
 
 shinyServer(function(input, output, session){
-   output$x <- renderUI({
-      x <- ifelse(!is.null(input$x), input$x, 0)
-      zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
-      from <- x-2/2^zoom
-      to <- x+2/2^zoom
-      sliderInput("x", "X", min=from, max=to, value=x, step=.025/2^zoom)      
-   })
-   output$y <- renderUI({
-      y <- ifelse(!is.null(input$y), input$y, 0)
-      zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
-      from <- y-2/2^zoom
-      to <- y+2/2^zoom
-      sliderInput("y", "Y", min=from, max=to, value=y, step=.025/2^zoom)
-   })
-   output$zoom <- renderUI({
-      zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
-      if(zoom < 10){
-         from <- 0
-         to <- 20
-      } else if(zoom > 30){
-         from <- 20
-         to <- 40
-      } else {
-         from <- zoom - 10
-         to <- zoom + 10
-      }
-      sliderInput("zoom", "Zoom", from, to, value=zoom, step=.25)
-   })
-   output$mandelbrot <- renderPlot({
-      step <- 25
-      reps <- 400
-      n <- 400
-      x <- ifelse(!is.null(input$x), input$x, 0)
-      y <- ifelse(!is.null(input$y), input$y, 0)
-      zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
-      w <- 4/2^zoom
-      h <- 4/2^zoom
-      
-      
-      C <- complex(imag=rep(seq(y - h/2, y + h/2, length.out=n), each=n),
-                   real=rep(seq(x - w/2, x + w/2, length.out=n), n))
-      Z <- rep(0, n*n)
-      diverge <- rep(0, n*n)
-      
-      j <- 0
-      upper <- 0
-      while(j < reps){
-         upper <- ifelse(upper+step < reps, upper+step, reps)
-         empty <- diverge==0
-         diverge.temp <- diverge[empty]
-         Z.temp <- Z[empty]
-         C.temp <- C[empty]
-         for(i in (j+1):upper){
-            Z.temp <- Z.temp^2+C.temp
-            diverge.temp[diverge.temp == 0 & Mod(Z.temp) >= 2] <- i+1
+    # Get the x coordiate from the click
+    x <- reactive({
+        if(is.null(input$click)){
+            return(0)
+        }
+        input$click$x
+    })
+    # Get the y coordinate from the click
+    y <- reactive({
+        if(is.null(input$click)){
+            return(0)
+        }
+        input$click$y
+    })
+    # Output x coordinate to UI
+    output$x <- renderText({
+        paste("x:", x())
+    })
+    # Output y coordinate to UI
+    output$y <- renderText({
+        paste("y:", y())
+    })
+    # Zoom slider, the mix and max values will update when the button is clicked
+    # This helps avoid over sensitivity in the slider
+    output$zoom <- renderUI({
+        input$update
+        isolate({
+            zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
+            if(zoom < 10){
+                min.zoom <- 0
+                max.zoom <- 20
+            } else if(zoom > 30){
+                min.zoom <- 20
+                max.zoom <- 40
+            } else {
+                min.zoom <- zoom - 10
+                max.zoom <- zoom + 10
+            }
+            sliderInput("zoom", "Zoom:", min.zoom, max.zoom, value=zoom, step=.25)
+        })
+    })
+    # Output the mandelbrot drawying
+    output$mandelbrot <- renderPlot({
+        input$update
+        isolate({
+            #parameters
+            step <- 25
+            reps <- 400
+            n <- 400
+            zoom <- ifelse(!is.null(input$zoom), input$zoom, 0)
+            x <- x()
+            y <- y()
+            w <- 4/2^zoom
+            h <- 4/2^zoom
             
-         }
-         diverge[empty] <- diverge.temp
-         Z[empty] <- Z.temp
-         j <- upper
-      }
-      diverge[diverge!=0] <- 1 + diverge[diverge!=0] - min(diverge[diverge!=0])
-      diverge <- diverge+1
-      maxD <- max(diverge)
-      diverge.m <- matrix(diverge, n, n)
-      par(mar=c(2, 2, .5, .5))
-      image(seq(x - w/2, x + w/2, length.out=n), seq(y - h/2, y + h/2, length.out=n), 
-            diverge.m, col=c("black", topo.colors(maxD-1)), xlab="", ylab="")
-      
-   })
-   
+            #starting point
+            C <- complex(imag=rep(seq(y - h/2, y + h/2, length.out=n), each=n),
+                         real=rep(seq(x - w/2, x + w/2, length.out=n), n))
+            Z <- rep(0, n*n)
+            diverge <- rep(0, n*n)
+            
+            j <- 0
+            upper <- 0
+            while(j < reps){
+                #perform some house keeping after every 'step' repetitions, for performace improvement
+                upper <- ifelse(upper+step < reps, upper+step, reps)
+                empty <- diverge==0
+                diverge.temp <- diverge[empty]
+                Z.temp <- Z[empty]
+                C.temp <- C[empty]
+                
+                for(i in (j+1):upper){
+                    #do the fractal caluculation
+                    Z.temp <- Z.temp^2+C.temp
+                    diverge.temp[diverge.temp == 0 & Mod(Z.temp) >= 2] <- i
+                }
+                
+                diverge[empty] <- diverge.temp
+                Z[empty] <- Z.temp
+                j <- upper
+            }
+            
+            #perform the rendering
+            diverge <- diverge+1
+            maxD <- max(diverge)
+            diverge.m <- matrix(diverge, n, n)
+            par(mar=c(2, 2, .5, .5))
+            jet.colors = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", 
+                                            "yellow", "#FF7F00", "red", "#7F0000"))
+            image(seq(x - w/2, x + w/2, length.out=n), seq(y - h/2, y + h/2, length.out=n), 
+                  diverge.m, col=c("black", jet.colors(reps)), xlab="", ylab="")
+        })
+        
+    })
+
 })
--- ui.R
+++ ui.R
@@ -1,17 +1,24 @@
 library(shiny)
-
-shinyUI(fluidPage(
-   titlePanel("Mandelbrot"),
-   
-   sidebarLayout(
-      sidebarPanel(
-         uiOutput("zoom"),
-         uiOutput("x"),
-         uiOutput("y"),
-         submitButton("Submit")
-      ),
-      mainPanel(
-         plotOutput('mandelbrot', height=425, width=425)
-      )
+shinyUI(pageWithSidebar(
+   headerPanel("Mandelbrot"),
+   sidebarPanel(
+      h3("Welcome to the Shiny Mandelbrot app!"),
+      p(paste("Notice the slider below, this controls the zoom of the view.",
+                 "Move it to the right to zoom in, or the left to zoom out. ",
+                 "The x and y coordinates of the center of the view are printed",
+                 "below the zoom slider.  To control the x and y coordinates ",
+                 "click anywhere on the image, the coordinates will be set to",
+                 "the location clicked!")),
+      p("When you are ready, click the Update! button to re-render the image with the new parameters."),
+      div(HTML("<hr>")),
+      uiOutput("zoom"),
+      div(HTML("<hr>")),
+      textOutput("x"),
+      textOutput("y"),
+      div(HTML("<hr>")),
+      actionButton("update", "Update!")
+   ),
+   mainPanel(
+      plotOutput("mandelbrot", width="450", height="450", clickId="click")
    )
 ))
